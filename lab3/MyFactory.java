52bdff77ce23a51cd26a8dcaff7e5e1de2ae094f
Татьяна <43996253+u-tain@users.noreply.github.com>
2023-03-02 17:06:45 +0300

    Update MyFactory.java

--- lab3/MyFactory.java
+++ lab3/MyFactory.java
@@ -18,7 +18,7 @@ public class MyFactory extends AbstractCandidateFactory<double[]> {
 
         // your implementation:
         for (int i = 0; i < solution.length; i++) {
-            solution[i] = random.nextInt(10 + 1) - 5;
+            solution[i] = random.nextDouble() * 11 - 5;
         }
         return solution;
     }
