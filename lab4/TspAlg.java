915e4d6b52405f56b17b2904a11be7233fb1dfb6
Татьяна <43996253+u-tain@users.noreply.github.com>
2023-03-15 22:54:55 +0300

    Add files via upload

---
+++ lab4/TspAlg.java
@@ -0,0 +1,48 @@
+package lab3;
+
+import org.uncommons.watchmaker.framework.*;
+import org.uncommons.watchmaker.framework.operators.EvolutionPipeline;
+import org.uncommons.watchmaker.framework.selection.RouletteWheelSelection;
+import org.uncommons.watchmaker.framework.termination.GenerationCount;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class TspAlg {
+
+    public static void main(String[] args) {
+        String problem = "xqf131.tsp"; // name of problem or path to input file
+
+        int populationSize = 100; // size of population
+        int generations = 10000; // number of generations
+
+        Random random = new Random(); // random
+
+        CandidateFactory<TspSolution> factory = new TspFactory(); // generation of solutions
+
+        ArrayList<EvolutionaryOperator<TspSolution>> operators = new ArrayList<EvolutionaryOperator<TspSolution>>();
+        operators.add(new TspCrossover()); // Crossover
+        operators.add(new TspMutation()); // Mutation
+        EvolutionPipeline<TspSolution> pipeline = new EvolutionPipeline<TspSolution>(operators);
+
+        SelectionStrategy<Object> selection = new RouletteWheelSelection(); // Selection operator
+
+        FitnessEvaluator<TspSolution> evaluator = new TspFitnessFunction(problem); // Fitness function
+
+        EvolutionEngine<TspSolution> algorithm = new SteadyStateEvolutionEngine<TspSolution>(
+                factory, pipeline, evaluator, selection, populationSize, false, random);
+
+        algorithm.addEvolutionObserver(new EvolutionObserver() {
+            public void populationUpdate(PopulationData populationData) {
+                double bestFit = populationData.getBestCandidateFitness();
+                System.out.println("Generation " + populationData.getGenerationNumber() + ": " + bestFit);
+                TspSolution best = (TspSolution)populationData.getBestCandidate();
+                System.out.println("\tBest solution = " + best.ToString());
+            }
+        });
+
+        TerminationCondition terminate = new GenerationCount(generations);
+        algorithm.evolve(populationSize, 50, terminate);
+    }
+}
